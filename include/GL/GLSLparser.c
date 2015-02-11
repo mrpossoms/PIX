b382ea3d5ada65bd58bf00d499e00831438974aa
Kirk Roerig <mr.possoms@gmail.com>
2015-02-11 02:04:14 -0500

    Finished writing GLSL declaration parser... Completely untested

--- include/GL/GLSLparser.c
+++ include/GL/GLSLparser.c
@@ -1,6 +1,8 @@
 #include "GLSLparser.h"
+#include <string.h>
+#include <unistd.h>
 
-static const char* GLSL_TYPE_NAMES = {
+static const char* GLSL_TYPE_NAMES[] = {
 	// vectors are suffixed with a number
 	// to indicate number of elements
 	"vec",
@@ -19,6 +21,32 @@ static const char* GLSL_TYPE_NAMES = {
 	"bool",
 	"int",
 	"uint",
+
+	"sampler",
+};
+
+static const char* GLSL_TYPE_DATA_TYPES[] = {
+	PIX_GLSL_FLOAT, //"vec",
+	PIX_GLSL_FLOAT, //"dvec",
+	PIX_GLSL_INT,   //"ivec",
+	PIX_GLSL_UINT,  //"uvec",
+	PIX_GLSL_INT,   //"bvec",
+
+	PIX_GLSL_FLOAT, //"mat",
+
+	PIX_GLSL_FLOAT, //"float",
+	PIX_GLSL_FLOAT, //"double",
+	PIX_GLSL_INT,   //"bool",
+	PIX_GLSL_INT,   //"int",
+	PIX_GLSL_UINT,  //"uint",
+
+	PIX_GLSL_INT,   //"sampler",
+};
+
+static const char* GLSL_PRECISIONS[] = {
+	"lowp",
+	"highp",
+	"mediump",
 };
 
 // Side
@@ -45,25 +73,176 @@ static const char* GLSL_TYPE_NAMES = {
 // | Type name |[ Dimension ]
 
 // Data type
-// | Base type |[ Array dimensions ]
+// | Base type |
  
 // Statement
-// | Decl type |[ Precision ]| data type || identifier |;
+// | Decl type |[ Precision ]| data type || identifier |[ Array dimensions ];
 
 
 char* _glsl_genNextToken(struct PixGLSLParseState* state)
 {
+	char* str = NULL;
+	if(!state->parsingBegan){
+		str = state->src;
+		state->parsingBegan = 1;
+	}
 
+	return strtok(str, " \t\n[]");
+}
+
+int _glsl_char2elements(char c)
+{
+	switch(token[prefixLen]){
+		case '2':
+			return 2;
+		case '3':
+			return 3;
+		case '4':
+			return 4;
+		default:
+			return 0;
+	}
+}
+
+int _glsl_parseDataType(char* token, struct PixGLSLParameter* param)
+{
+	int i, prefixLen = 0, noPrefix = 1;
+
+	// determine the prefix of the type
+	for(i = 0; i < sizeof(GLSL_TYPE_NAMES) / sizeof(char*); ++i){
+		prefixLen = strlen(GLSL_TYPE_NAMES[i]);
+
+		if(!strncmp(GLSL_TYPE_NAMES[i], token, prefixLen)){
+			noPrefix = 0;
+			break; // we found a corresponding type prefix
+		}
+	}
+
+	param->dataType = GLSL_TYPE_DATA_TYPES[i];
+
+	// a type is being defined that will have a dimension term
+	if(i < 5){ // VECTOR
+		param->width    = 1;
+		param->height   = _glsl_char2elements(token[prefixLen]);
+	}
+	else if(i == 5){ // MATRIX
+		param->width    = _glsl_char2elements(token[prefixLen]);
+		param->height   = _glsl_char2elements(token[prefixLen + 2]);
+	}
+	else{
+		return 0;
+	}
+
+	// make sure elements were returned
+	if(!param->width * param->height){
+		return PIX_GLSL_MSG_ERR_BAD_DIMENSION;
+	}
+
+	return 0;
+}
+
+int _glsl_parseIdentifier(struct PixGLSLParseState* state, char* token)
+{
+	struct PixGLSLParameter* current = &state->currentParameter;	
+	int i = 0, tokenLen = strlen(token);
+
+	// non array?
+	if(token[tokenLen] == ';'){
+		memcpy(current->name, token, tokenLen);
+		return 0;
+	}
+
+	// the statement hasn't been terminated in the identifier
+	// an array declaration must have taken place
+	do{
+		token = _glsl_genNextToken(state);
+		sscanf(token, "%d", &current->arrayLens[i++]);
+		if(i >= 3) return PIX_GLSL_MSG_ERR_ARRAY_TOO_MANY_DIMS;
+	}while(token[0] != ';');
+
+	return 0;
+}
+
+int _glsl_parseDecl(struct PixGLSLParseState* state)
+{
+	struct PixGLSLParameter* current = &state->currentParameter;
+	int hasDeclType   = current->type;
+	int hasPrecision  = current->precision;
+	int hasDataType   = current->elements;
+	int hasIdentifier = strlen(current->name);
+
+	if(hasIdentifier) return PIX_GLSL_MSG_DONE_PARSING_DECL;
+
+	char* token = _glsl_genNextToken(state);
+
+	if(!token) return PIX_GLSL_MSG_DONE_PARSING;
+
+	if(!hasDeclType){
+		if(!strcmp(token, "attribute")) current->type = PIX_GLSL_ATTRIB;
+		if(!strcmp(token, "uniform"))   current->type = PIX_GLSL_UNI;
+
+		if(current->type == PIX_GLSL_NONE){
+			return PIX_GLSL_MSG_DONE_PARSING;
+		}
+	}
+	else if(!hasPrecision && !hasDataType){
+		int i = 0;
+
+		// iterate over all precicion strings, try to find one that matches
+		current->precision = PIX_GLSL_NOP; // otherwise, indicate that none was set
+		for(i = 0; i < sizeof(GLSL_PRECISIONS) / sizeof(char*); ++i){
+			if(!strcmp(token, GLSL_PRECISIONS[i])){
+				current->precision = (enum PixGLSLPrecision)i;
+				break;
+			}
+		}
+	}
+	else if(!hasDataType){
+		int ret = _glsl_parseDataType(token, currentParameter);
+		if(ret) return ret;
+	}
+	else if(!hasIdentifier){
+		int ret = _glsl_parseIdentifier(state, token);
+		if(ret) return ret;
+	}
+
+	return 0;
+}
+
+void _glsl_initParameter(struct PixGLSLParameter* param)
+{
+	bzero(param, sizeof(struct PixGLSLParameter));
+	param->precision = PIX_GLSL_UNKNOWNP;
 }
 
 int PixGLSLParseSource(
 	struct PixGLSLParseState* state,
 	const char* src)
 {
-	state->src      = src;
-	state->srcIndex = 0;
+	int ret = 0;
 
+	state->src          = malloc(strlen(src) + 1);
+	state->parsingBegan = 0;
 
+	strcpy(state->src, src);
+	_glsl_initParameter(state);
+
+	do
+	{
+		ret = _glsl_parseDecl(state);
+
+		if(ret == PIX_GLSL_MSG_DONE_PARSING_DECL){
+			if(state->current.type == PIX_GLSL_ATTRIB){
+				state->attributes[state->attributeCount++] = state->current;
+			}
+			else if(state->current.type == PIX_GLSL_UNI){
+				state->uniforms[state->uniformCount++] = state->current;
+			}
+		}
+	}
+	while(ret != PIX_GLSL_MSG_DONE_PARSING);
+
+	free(state->src);
 
 	return 0;
 }
--- include/GL/GLSLparser.h
+++ include/GL/GLSLparser.h
@@ -1,28 +1,53 @@
 #ifndef PIX_IMP_OPENGL_GLSL_PARSER
 #define PIX_IMP_OPENGL_GLSL_PARSER
 
+#define PIX_GLSL_MSG_DONE_PARSING            -1
+#define PIX_GLSL_MSG_ERR_BAD_DIMENSION       -2
+#define PIX_GLSL_MSG_ERR_ARRAY_TOO_MANY_DIMS -3
+#define PIX_GLSL_MSG_DONE_PARSING_DECL       -4
+
 enum PixGLSLParamType{
+	PIX_GLSL_NONE = 0,
 	PIX_GLSL_ATTRIB,
 	PIX_GLSL_UNI,
 };
 
+enum PixGLSLPrecision{
+	PIX_GLSL_UNKNOWNP = -2;
+	PIX_GLSL_NOP      = -1,
+	PIX_GLSL_LOWP     =  0,
+	PIX_GLSL_HIGHP    =  1,
+	PIX_GLSL_MEDIUMP  =  2,
+};
+
+enum PixGLSLPrimitiveType{
+	PIX_GLSL_FLOAT,
+	PIX_GLSL_INT,
+	PIX_GLSL_UINT,
+};
+
 struct PixGLSLParameter{
-	char                  name[64];
-	unsigned int          bytes;
-	unsigned int          elements;
-	enum PixGLSLParamType type;
+	char                      name[64];
+	unsigned int              bytes;
+	unsigned int              width, height;
+	unsigned int              arrayLens[3];
+	enum PixGLSLPrimitiveType dataType;
+	enum PixGLSLPrecision     precision;
+	enum PixGLSLParamType     type;
 };
 
 struct PixGLSLParseState{
 	// variables for scanning the source
 	const char*  src;
-	unsigned int srcIndex;
+	unsigned int parsingBegan;
 
 	struct PixGLSLParameter  attributes[64];
 	unsigned int             attributeCount;
 
 	struct PixGLSLParameter* uniforms[64];
 	unsigned int             uniformCount;
+
+	struct PixGLSLParameter  currentParameter;
 };
 
 int PixGLSLParseSource(
